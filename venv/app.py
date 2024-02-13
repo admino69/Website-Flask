9abae51d32594c70ea604df055b5f5c7eb1ad768
admino69 <140599622+admino69@users.noreply.github.com>
2024-02-13 10:28:51 +0100

    Update app.py

--- venv/app.py
+++ venv/app.py
@@ -1,5 +1,19 @@
 from flask import Flask
 
+#Create template using HTML and CSS --
+from flask import Flask, render_template
+
+#-----------
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#------------
 app = Flask(__name__)
 @app.route("/")
 def home():
